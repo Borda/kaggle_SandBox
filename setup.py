4c75bf391885fd2897253922fb1d21fead04deed
Jirka <jirka.borovec@seznam.cz>
2022-06-20 23:12:06 +0200

    setup in main

--- setup.py
+++ setup.py
@@ -47,43 +47,44 @@ def _load_long_description(homepage: str, version: str):
 # what happens and to non-engineers they won't know to look in init ...
 # the goal of the project is simplicity for researchers, don't want to add too much
 # engineer specific practices
-setup(
-    name="kaggle-sandbox",
-    version=_about.__version__,
-    description=_about.__docs__,
-    author=_about.__author__,
-    author_email=_about.__author_email__,
-    url=_about.__homepage__,
-    license=_about.__license__,
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    long_description=_load_long_description(_about.__homepage__, _about.__version__),
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    zip_safe=False,
-    keywords=["deep learning", "pytorch", "AI"],
-    python_requires=">=3.6",
-    setup_requires=[],
-    install_requires=_load_requirements(_PATH_ROOT),
-    project_urls={
-        "Source Code": _about.__homepage__,
-    },
-    classifiers=[
-        "Environment :: Console",
-        "Natural Language :: English",
-        # How mature is this project? Common values are
-        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
-        "Development Status :: 3 - Alpha",
-        # Indicate who your project is intended for
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Image Recognition",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        # Pick your license as you wish
-        # 'License :: OSI Approved :: BSD License',
-        "Operating System :: OS Independent",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3",
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        name="kaggle-sandbox",
+        version=_about.__version__,
+        description=_about.__docs__,
+        author=_about.__author__,
+        author_email=_about.__author_email__,
+        url=_about.__homepage__,
+        license=_about.__license__,
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        long_description=_load_long_description(_about.__homepage__, _about.__version__),
+        long_description_content_type="text/markdown",
+        include_package_data=True,
+        zip_safe=False,
+        keywords=["deep learning", "pytorch", "AI"],
+        python_requires=">=3.6",
+        setup_requires=[],
+        install_requires=_load_requirements(_PATH_ROOT),
+        project_urls={
+            "Source Code": _about.__homepage__,
+        },
+        classifiers=[
+            "Environment :: Console",
+            "Natural Language :: English",
+            # How mature is this project? Common values are
+            #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+            "Development Status :: 3 - Alpha",
+            # Indicate who your project is intended for
+            "Intended Audience :: Developers",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Scientific/Engineering :: Image Recognition",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            # Pick your license as you wish
+            # 'License :: OSI Approved :: BSD License',
+            "Operating System :: OS Independent",
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            "Programming Language :: Python :: 3",
+        ],
+    )
