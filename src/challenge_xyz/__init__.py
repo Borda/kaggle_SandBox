19195041b47729f6e6cc29a3ecded0d90eaa5180
Jirka B <j.borovec+github@gmail.com>
2024-09-17 22:15:50 +0200

    fixing

--- src/challenge_xyz/__init__.py
+++ src/challenge_xyz/__init__.py
@@ -2,7 +2,7 @@
 
 import os
 
-from challenge_xyz.__about__ import *  # noqa: F401, F403
+from challenge_xyz.__about__ import *  # noqa: F403
 
 _PATH_PACKAGE = os.path.realpath(os.path.dirname(__file__))
 _PATH_PROJECT = os.path.dirname(_PATH_PACKAGE)
