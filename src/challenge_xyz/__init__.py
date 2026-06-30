b2d0b0e43b0fe3e255ed22a8dff483b4a2847462
jirka <6035284+Borda@users.noreply.github.com>
2026-06-30 14:32:33 +0200

    Simplify `__init__.py` by using wildcard imports and cleaning up `__all__`.

--- src/challenge_xyz/__init__.py
+++ src/challenge_xyz/__init__.py
@@ -1,16 +1,10 @@
 """Kaggle competition template helpers."""
 
-from challenge_xyz.__about__ import __author__, __author_email__, __docs__, __homepage__, __license__, __version__
+from challenge_xyz.__about__ import *  # noqa: F403
 from challenge_xyz.utils import CsvShape, assert_same_csv_shape, read_csv_shape
 
 __all__ = [
     "CsvShape",
-    "__author__",
-    "__author_email__",
-    "__docs__",
-    "__homepage__",
-    "__license__",
-    "__version__",
     "assert_same_csv_shape",
     "read_csv_shape",
 ]
